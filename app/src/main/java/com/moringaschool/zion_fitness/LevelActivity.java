7b911551e3eab024fb92b3330549974ffa6bbdb4
anzal <anzaldidah@gmail.com>
2021-06-09 21:35:55 +0300

    created Activity for workout levels

--- app/src/main/java/com/moringaschool/zion_fitness/LevelActivity.java
+++ app/src/main/java/com/moringaschool/zion_fitness/LevelActivity.java
@@ -2,13 +2,45 @@ package com.moringaschool.zion_fitness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LevelActivity extends AppCompatActivity {
+
+    private Button mhard,measy,mintermediate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
+
+        mhard=findViewById(R.id.hard);
+        measy=findViewById(R.id.easy);
+        mintermediate=findViewById(R.id.intermediate);
+
+
+        mhard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LevelActivity.this,HardActivity.class));
+            }
+        });
+
+
+        measy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LevelActivity.this, EasyActivity.class));
+            }
+        });
+
+        mintermediate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LevelActivity.this, IntermediateActivity.class));
+            }
+        });
     }
 }
