df1100da7079d36f665d522d7ed14b9b0ff450f2
anzal <anzaldidah@gmail.com>
2021-06-10 05:53:07 +0300

    commiting unversioned files

--- app/src/main/java/com/moringaschool/zion_fitness/HardActivity.java
+++ app/src/main/java/com/moringaschool/zion_fitness/HardActivity.java
@@ -2,13 +2,39 @@ package com.moringaschool.zion_fitness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HardActivity extends AppCompatActivity {
+
+    private Button day1,day2,day3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hard);
+
+
+        day1=findViewById(R.id.day1);
+
+
+        day1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HardActivity.this,Day1Activity.class));
+            }
+        });
+    }
+
+    public static class Day1Activity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_day1);
+        }
     }
 }
